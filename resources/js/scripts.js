bd8040389855ab1418a4b66968546f45e4716f4a
SarahIngold <singold@mycollarts.edu.au>
2022-08-16 21:03:20 +1000

    Added Lightbox functionality

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,13 +1,19 @@
+/* this is to open and close the nav */
+
 function openNav() {
-    
+
     let toggler = document.querySelector('.nav-prompt');
 
     let navigation = document.querySelector('.main-nav');
 
-    toggler.onclick = function() {
+    toggler.onclick = function () {
         navigation.classList.toggle('open');
     }
 
 }
 
 openNav();
+
+// this is for the gallery
+
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
